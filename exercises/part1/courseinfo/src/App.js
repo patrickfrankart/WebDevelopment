c2eb92977e89973a1415984427b0341d1723b097
Patrick Frankart <patrick.frankart@gmail.com>
2022-03-09 11:21:41 -0500

    Exercise 1.2.

--- exercises/part1/courseinfo/src/App.js
+++ exercises/part1/courseinfo/src/App.js
@@ -24,20 +24,23 @@ function Header(props) {
 }
 
 function Content(props) {
-  return (<>  <p>
-    {props.part1} {props.exercises1}
-  </p>
-  <p>
-    {props.part2} {props.exercises2}
-  </p>
-  <p>
-    {props.part3} {props.exercises3}
-  </p> </>);
+  return (
+  <div>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part2} exercises={props.exercises2} />
+  </div>);
 }
 
 function Total(props) {
   return (<p> 
     Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+  </p>);
+}
+
+function Part(props) {
+  return (<p>
+    {props.part} {props.exercises}
   </p>);
 }
 
