6fc833a60c15faac446ee8b0fe50fb9b56bb6a04
Patrick Frankart <patrick.frankart@gmail.com>
2022-03-09 11:17:16 -0500

    Exercise 1.1.

--- exercises/part1/courseinfo/src/App.js
+++ exercises/part1/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -12,21 +12,33 @@ function App() {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p> 
-        Number of exercises {exercises1 + exercises2 + exercises3}
-      </p>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   );
+}
+
+function Header(props) {
+  return (<h1>{props.course}</h1>);
+}
+
+function Content(props) {
+  return (<>  <p>
+    {props.part1} {props.exercises1}
+  </p>
+  <p>
+    {props.part2} {props.exercises2}
+  </p>
+  <p>
+    {props.part3} {props.exercises3}
+  </p> </>);
+}
+
+function Total(props) {
+  return (<p> 
+    Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+  </p>);
 }
 
 export default App;
